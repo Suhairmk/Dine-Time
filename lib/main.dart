bad2810a934742eb308c64a5f9a243e7215bd9d7
vaishnav <vaishnavjayaprakash28.com>
2025-06-06 11:09:53 +0530

    authsec

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@ import 'package:dinetime/app/constant/app_theme.dart';
 import 'package:dinetime/app/controllers/theme_controller.dart';
 import 'package:dinetime/app/routes/app_routes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localization/flutter_localization.dart';
+
 import 'package:get/get.dart';
 
 // Controller to handle theme mode with GetX
 
-void main()async {
-  WidgetsFlutterBinding.ensureInitialized();
-    await FlutterLocalization.instance.ensureInitialized();
+void main() {
   runApp(MyApp());
 }
 
